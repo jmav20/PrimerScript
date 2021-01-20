52e792187c1542421297784db36166384d7ba8b6
jmav20 <77193747+jmav20@users.noreply.github.com>
2021-01-20 17:19:54 +0100

    Ejercicio 1.2 y 1.3

--- myscript.js
+++ myscript.js
@@ -1,1 +1,20 @@
 alert("El primer codigo en JavaScript")
+
+const celta={
+    day:20,
+    month: 'Abril'
+}
+
+console.log('Mi cumpleaños es el ' + celta.day + ' de ' + celta.month)
+
+
+const data = ["hola",2,5,"adios"]
+let resultado = data[1]>data[2]
+
+console.log('El numero ' + data[1] + ' es mayor que ' + data[2] + ' = ' + resultado)
+
+console.log('suma =' + data[2]+data[1])
+console.log('resta =' + (data[2]-data[1]))
+console.log('multiplicacion =' + data[2]*data[1])
+console.log('division =' + data[2]/data[1])
+console.log('resto =' + data[2]%data[1])
