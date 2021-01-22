5df44e1e7a4150c7f09ab38507e4e675fb59da6b
jmav20 <77193747+jmav20@users.noreply.github.com>
2021-01-22 06:10:14 +0100

    Ejercicio 1.10

---
+++ Ejercicio 1.10/myscript.js
@@ -0,0 +1,9 @@
+const number =[253,8575,1,20,562,1233,25,27,258,254,7485,2683];
+
+//filtar pares
+const par = number.filter(num => num%2 == 0);
+alert(par);
+
+//filtrar impares
+const impar = number.filter(num => num%2 !=0);
+alert(impar);
